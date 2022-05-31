78bbdd4879c9926be9fe572c6b244dec9fc08016
thundermiracle <cissoliu@gmail.com>
2022-05-31 23:30:43 +0900

    feat: fix accesssibility & seo problem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>UnoCSS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Example of Next.js with UnoCSS" />
+        <meta
+          name="keywords"
+          content="next.js, unocss, css, scss, sass, less, stylus, javascript, typescript"
+        />
       </Head>
       <Component {...pageProps} />
     </>
